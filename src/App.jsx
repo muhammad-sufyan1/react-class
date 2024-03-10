f9f821e9064819f504fc4be8d1447e5e26af15d7
Muhammad Sufyan <sufyan.vu07@gmail.com>
2024-03-10 12:09:02 +0500

    basic counter logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 
 
 function App() {
 
+  let [counter, setCounter] = useState(0);
+  
+  const addValue = () =>{
+    if (counter < 10 ) {
+      
+      setCounter(counter + 1)
+      
+    }
+  }
+  
+  const removeValue = () =>{
+    if (counter > 0) {
+      setCounter(counter -1)
+      
+    }
+  }
+
   return (
     <>
-     
-      <h1>React | Chai aur Code</h1>
+      <h1>React</h1>
+      <h2>Counter App: {counter}</h2>
+      <button onClick={addValue}>Add Value: {counter}</button>
+      <br></br>
+      <br></br>
+      <button onClick={removeValue}>Remove Value: {counter}</button>
      
     </>
   )
